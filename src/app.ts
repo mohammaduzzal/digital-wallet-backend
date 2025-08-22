810aa05a52e5c58fef03550516928f6f10af20be
mohammaduzzal <aalp72142@gmail.com>
2025-08-22 19:28:29 +0600

    added cors url

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,17 @@ import {router} from "./app/routes"
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import { notFound } from "./app/middlewares/notFound";
 import cookieParser from "cookie-parser";
+import { envVars } from "./app/config/env";
 
 
 const app = express()
 
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin : envVars.FRONTEND_URL,
+    credentials : true
+}));
 
 
 app.use("/api/v1", router)
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -12,12 +12,13 @@ interface EnvConfig {
     JWT_REFRESH_SECRET: string;
     JWT_ACCESS_EXPIRES: string;
     JWT_REFRESH_EXPIRE: string;
+    FRONTEND_URL: string;
 }
 
 
 const loadEnvVariables = (): EnvConfig => {
 
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPTJS_SALT_ROUND", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRE",]
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV", "BCRYPTJS_SALT_ROUND", "JWT_ACCESS_SECRET", "JWT_ACCESS_EXPIRES", "JWT_REFRESH_SECRET", "JWT_REFRESH_EXPIRE","FRONTEND_URL"]
 
 
     requiredEnvVariables.forEach(key => {
@@ -38,6 +39,7 @@ const loadEnvVariables = (): EnvConfig => {
         JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
         JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
         JWT_REFRESH_EXPIRE: process.env.JWT_REFRESH_EXPIRE as string,
+        FRONTEND_URL: process.env.FRONTEND_URL as string,
 
 
 
